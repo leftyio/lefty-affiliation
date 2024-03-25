af20279aacd8c9d6d02ed31ad13174e320daee1a
Hadrien Lejard <hadrien@lefty.io>
2024-03-25 14:10:26 +0100

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "affiliation.js",
+    filename: "index.min.js",
     library: "lefty",
     libraryTarget: "umd",
   },
